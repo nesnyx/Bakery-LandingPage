7dd3085d13fd4bbb8cb54b625a19d16c7e27919a
HeyRaa <89240098+nesnyx@users.noreply.github.com>
2022-08-05 15:50:01 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set("Asia/Jakarta");
+error_reporting(0);
+
+	$user = 'root'; $pass = '';
+	try{
+		$config = new PDO('mysql:host=localhost;dbname=db_roti;', $user,$pass);
+		//echo 'sukses';
+	}catch(PDOException $e){
+		echo 'KONEKSI GAGAL' .$e -> getMessage();
+	}
+	$view = 'fungsi/view/view.php'; // DIREKTORI PROSES SELECT /
